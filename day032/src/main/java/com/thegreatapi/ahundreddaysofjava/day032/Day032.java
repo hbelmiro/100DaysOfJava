706cfa57720cbb0debdb3c3e6aefd631390ebf7c
hbelmiro <helber.belmiro@gmail.com>
2021-06-20 12:26:28 -0300

    Day 32

---
+++ day032/src/main/java/com/thegreatapi/ahundreddaysofjava/day032/Day032.java
@@ -0,0 +1,26 @@
+package com.thegreatapi.ahundreddaysofjava.day032;
+
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+
+public class Day032 {
+
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        String result = map(randomNumber());
+        System.out.println(result);
+    }
+
+    private static String map(int number) {
+        // Requires JDK 12
+        return switch (number) {
+            case 1 -> "one";
+            case 2 -> "two";
+            case 3 -> "three";
+            default -> "unknown";
+        };
+    }
+
+    private static int randomNumber() {
+        return new SecureRandom().nextInt(4);
+    }
+}
