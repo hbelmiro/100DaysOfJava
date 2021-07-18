6fd64a911b37d701a6eea8ccd7100b9d0a6a590e
hbelmiro <helber.belmiro@gmail.com>
2021-07-18 20:32:24 -0300

    Day 60

--- days/day060/src/main/java/com/thegreatapi/ahundreddaysofjava/day060/Day060.java
+++ days/day060/src/main/java/com/thegreatapi/ahundreddaysofjava/day060/Day060.java
@@ -1,26 +1,27 @@
-package com.thegreatapi.ahundreddaysofjava.day059;
+package com.thegreatapi.ahundreddaysofjava.day060;
 
 import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
-public class Day059 {
+import static java.util.function.Predicate.not;
+
+public class Day060 {
 
     public static void main(String[] args) {
-        /*
-         A Predicate<T> is the same as Function<T, Boolean>.
-         It consumes a T and returns a Boolean.
-         */
         Predicate<Integer> isEven = intValue -> intValue % 2 == 0;
+        Predicate<Integer> isPositive = intValue -> intValue > 0;
 
         List<Integer> numbers = getNumbers();
+
+        // Prints negative odd numbers and positive even numbers.
         numbers.stream()
-               .filter(isEven)
+               .filter(isEven.and(isPositive).or(not(isEven).and(not(isPositive))))
                .forEach(System.out::println);
     }
 
     private static List<Integer> getNumbers() {
-        return IntStream.rangeClosed(1, 100).boxed().collect(Collectors.toList());
+        return IntStream.rangeClosed(-20, 20).boxed().collect(Collectors.toList());
     }
 }
