c447ccaf6633181fbcd9c2b4ebbd3861b36e027a
hbelmiro <helber.belmiro@gmail.com>
2021-08-11 18:59:31 -0300

    Day 84

---
+++ days/day084/src/main/java/com/thegreatapi/ahundreddaysofjava/day084/Day084.java
@@ -0,0 +1,28 @@
+package com.thegreatapi.ahundreddaysofjava.day084;
+
+import org.eclipse.microprofile.faulttolerance.Retry;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import java.time.temporal.ChronoUnit;
+
+@Path("/hello")
+public class Day084 {
+
+    private static int numberOfAttempts = 0;
+
+    @GET
+    @Retry(maxRetries = 3, delay = 2, delayUnit = ChronoUnit.SECONDS)
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello() {
+        numberOfAttempts++;
+
+        if (numberOfAttempts < 3) {
+            throw new RuntimeException("Emulates an error");
+        } else {
+            return "Hello after " + numberOfAttempts + " attempts";
+        }
+    }
+}
