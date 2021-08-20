d923bf4824166d8c20191f67919023d1fdcd5b6f
hbelmiro <helber.belmiro@gmail.com>
2021-08-20 18:46:06 -0300

    Day093

---
+++ days/day093/src/main/java/com/thegreatapi/ahundreddaysofjava/day093/Day093.java
@@ -0,0 +1,33 @@
+package com.thegreatapi.ahundreddaysofjava.day093;
+
+import java.time.Instant;
+import java.time.temporal.ChronoField;
+import java.util.List;
+
+public class Day093 {
+
+    public static void main(String[] args) {
+        List<String> myList = getList();
+
+        // If myList is null, a NullPointerException will be thrown
+        for (String s : myList) {
+            System.out.println(s);
+        }
+    }
+
+    private static List<String> getList() {
+        if (someCondition()) {
+            return List.of("a", "b", "c");
+        } else {
+//          Instead of returning null
+//          return null;
+
+//          Return an empty list, so the caller don't need to check if the returned list is not null
+            return List.of();
+        }
+    }
+
+    private static boolean someCondition() {
+        return Instant.now().get(ChronoField.MILLI_OF_SECOND) % 2 == 0;
+    }
+}
