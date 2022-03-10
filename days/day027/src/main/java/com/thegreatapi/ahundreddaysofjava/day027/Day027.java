0c9da3def26810ea72c509690becbfe129adbe1c
hbelmiro <helber.belmiro@gmail.com>
2022-03-10 20:36:28 -0300

    Improvements on days 27 and 28
    Signed-off-by: hbelmiro <helber.belmiro@gmail.com>

--- days/day027/src/main/java/com/thegreatapi/ahundreddaysofjava/day027/Day027.java
+++ days/day027/src/main/java/com/thegreatapi/ahundreddaysofjava/day027/Day027.java
@@ -18,7 +18,7 @@ public final class Day027 {
 
     private static List<Player> createList() {
         var messi = new Player("Lionel Messi", "PSG", "Argentina", 42);
-        var cr7 = new Player("Cristiano Ronaldo", "Juventus", "Portugal", 50);
+        var cr7 = new Player("Cristiano Ronaldo", "Manchester United", "Portugal", 50);
         var lukaku = new Player("Romelu Lukaku", "Chelsea", "Belgium", 41);
 
         return List.of(messi, cr7, lukaku);
--- days/day028/src/main/java/com/thegreatapi/ahundreddaysofjava/day028/Day028.java
+++ days/day028/src/main/java/com/thegreatapi/ahundreddaysofjava/day028/Day028.java
@@ -1,24 +1,25 @@
 package com.thegreatapi.ahundreddaysofjava.day028;
 
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
 
-public class Day028 {
+import static java.util.Comparator.comparing;
+
+public final class Day028 {
 
     public static void main(String[] args) {
-        Player messi = new Player("Lionel Messi", "Barcelona", "Argentina", 42);
-        Player cr7 = new Player("Cristiano Ronaldo", "Juventus", "Portugal", 50);
-        Player neymar = new Player("Neymar Jr.", "PSG", "Brazil", 41);
+        var messi = new Player("Lionel Messi", "PSG", "Argentina", 42);
+        var cr7 = new Player("Cristiano Ronaldo", "Manchester United", "Portugal", 50);
+        var lukaku = new Player("Romelu Lukaku", "Chelsea", "Belgium", 41);
 
-        List<Player> players = Arrays.asList(messi, cr7, neymar);
+        List<Player> players = Arrays.asList(messi, cr7, lukaku);
 
-        players.sort(Comparator.comparing(Player::numberOfGoals).reversed());
+        players.sort(comparing(Player::numberOfGoals).reversed());
 
         System.out.println("Top Scorers:");
         players.forEach(System.out::println);
     }
 
-    private record Player(String name, String club, String coutry, int numberOfGoals) {
+    private record Player(String name, String club, String country, int numberOfGoals) {
     }
 }
