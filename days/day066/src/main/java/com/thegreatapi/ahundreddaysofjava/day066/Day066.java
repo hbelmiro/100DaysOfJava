074db40eb1b318b9c76f09d63d4f213dc73b98e5
Helber Belmiro <helber.belmiro@gmail.com>
2023-09-19 19:22:33 -0300

    Upgraded to JDK 21 (#64)

--- days/day066/src/main/java/com/thegreatapi/ahundreddaysofjava/day066/Day066.java
+++ days/day066/src/main/java/com/thegreatapi/ahundreddaysofjava/day066/Day066.java
@@ -1,16 +1,10 @@
 package com.thegreatapi.ahundreddaysofjava.day066;
 
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.ToString;
-
 import java.time.LocalDate;
 
-@SuppressWarnings({"ClassCanBeRecord", "unused"})
 public class Day066 {
 
-    // Instead of doing:
+    /* Instead of doing:
 
     @AllArgsConstructor
     @ToString
@@ -24,7 +18,8 @@ public class Day066 {
         private final String country;
     }
 
-    // Just do: (requires JDK 16)
+    Just do: (requires JDK 16)
+    */
 
     record PersonPureJava(String name, LocalDate dateOfBirth, String country) {
     }
